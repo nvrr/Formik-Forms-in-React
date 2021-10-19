fd21f6c01d7cb19433140bab06ed74c0295d627e
nvrr <sureshvarma809@gmail.com>
2021-10-19 16:40:26 +0530

    React-Formik Form Advanced Validation and Reducing Boilerplate

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 // gh""
 export default function SampleForm() {
@@ -9,27 +10,33 @@ export default function SampleForm() {
         name: ""
     }
 
-    const validate = (values) => {
-        console.log(values);
-        let errors = {};
-        if (!values.email){
-            errors.email = "Required";
-        } else if(!/^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test(values.email)){
-           errors.email = "Invalid Email format";
-        }
-        if (!values.name){
-            errors.name = "Required";
-        }
-        return errors;
-    }
+    // const validate = (values) => {
+    //     console.log(values);
+    //     let errors = {};
+    //     if (!values.email){
+    //         errors.email = "Required";
+    //     } else if(!/^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test(values.email)){
+    //        errors.email = "Invalid Email format";
+    //     }
+    //     if (!values.name){
+    //         errors.name = "Required";
+    //     }
+    //     return errors;
+    // }
 
     const onSubmit = (values) => {
         console.log("form submitted",values);
     }
 
+    const validationSchema = Yup.object({
+        email: Yup.string().email("Invalid Email Format").required("Required"),
+        name: Yup.string().required("Required")
+    })
+
     const formik = useFormik({
         initialValues, //initialValues: initialValues,
-        validate,
+        //validate,
+        validationSchema,
         onSubmit,
     })
     console.log("Form",formik);
@@ -39,13 +46,10 @@ export default function SampleForm() {
       <form onSubmit={formik.handleSubmit}>
         <h1>Formik Feedback</h1>
         <label htmlFor="email">Your Email</label>
-        <input type="email" name="email" id="email" 
-            value={formik.values.email} 
-            onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type="email" name="email" id="email" {...formik.getFieldProps("email")}/>
         <p className="error">{formik.touched.email && formik.errors.email ? formik.errors.email : ""}</p>
         <label htmlFor="name">Your Name</label>
-        <input type="text" name="name" id="name" value={formik.values.name}
-           onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type="text" name="name" id="name" {...formik.getFieldProps("name")}/>
         <p className="error">{formik.touched.name && formik.errors.name ? formik.errors.name : ""}</p>
         <button type="submit">Submit</button>
         <p className="footer">Powered by Venki</p>
@@ -53,3 +57,5 @@ export default function SampleForm() {
     </div>
   );
 }
+// {...formik.getFieldProps("")} use it instead of below one 
+//value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}
